9d778faba34688ef692de097cc32e3d0a44f07a9
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2024-05-03 14:25:43 +0800

    feat: add table card

---
+++ src/components/TableCardContainer.tsx
@@ -0,0 +1,23 @@
+import { Grid } from "@chakra-ui/react";
+
+interface TableCardContainerProps {
+  children: JSX.Element;
+}
+
+const TableCardContainer = ({
+  children,
+  ...props
+}: TableCardContainerProps) => {
+  return (
+    <Grid
+      gridTemplateColumns={["1fr", "1fr 1fr", "1fr 1fr 1fr"]}
+      gap={"0.5rem"}
+      padding={"0.5rem"}
+      {...props}
+    >
+      {children}
+    </Grid>
+  );
+};
+
+export default TableCardContainer;
---
+++ src/components/TableCards.tsx
@@ -0,0 +1,37 @@
+import { Card, CardBody, Text, Box } from "@chakra-ui/react";
+import { flexRender } from "@tanstack/react-table";
+import { useContext } from "react";
+import { TableContext } from "./DataTableContext";
+
+const TableCards = () => {
+  const { table } = useContext(TableContext);
+
+  return (
+    <>
+      {table.getRowModel().rows.map((row) => {
+        return (
+          <Card key={crypto.randomUUID()}>
+            <CardBody display={'flex'} flexFlow={'column'} gap={'0.5rem'}>
+              {row.getVisibleCells().map((cell) => {
+                console.log(cell, "fkgodsokg");
+                return (
+                  <Box>
+                    <Text>{`${cell.column.id}: `}</Text>
+                    <Box>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </Box>
+                  </Box>
+                );
+              })}
+            </CardBody>
+          </Card>
+        );
+      })}
+    </>
+  );
+};
+
+export default TableCards;
