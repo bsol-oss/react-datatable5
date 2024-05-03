928c6652273fc5231647a0564a872ab686d411e9
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2024-05-03 13:24:54 +0800

    feat: add reset filtering

--- src/components/TableFilter.tsx
+++ src/components/TableFilter.tsx
@@ -2,38 +2,41 @@ import { Input, Text, Button } from "@chakra-ui/react";
 import { useDataTable } from "./useDataTable";
 
 const TableFilter = () => {
-  const { table, refreshData } = useDataTable();
+  const { table } = useDataTable();
 
   return (
     <>
       {table.getLeafHeaders().map((header) => {
-        console.log(header.column.getFilterValue(), "okgspokpsor");
         return (
           <>
             {header.column.getCanFilter() && (
               <>
                 <Text>{header.column.id}</Text>
                 <Input
-                  value={header.column.getFilterValue()}
+                  value={
+                    header.column.getFilterValue()
+                      ? String(header.column.getFilterValue())
+                      : ""
+                  }
                   onChange={(e) => {
-                    console.log(e, "gifdogj");
                     header.column.setFilterValue(e.target.value);
                     // console.log(value,"sdoafo")
                     // header.column.setFilterValue(value);
                   }}
                 />
-                <Button
-                  onClick={() => {
-                    refreshData();
-                  }}
-                >
-                  Filter
-                </Button>
               </>
             )}
           </>
         );
       })}
+
+      <Button
+        onClick={() => {
+          table.resetColumnFilters();
+        }}
+      >
+        Reset Filtering
+      </Button>
     </>
   );
 };
