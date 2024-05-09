4d00d59fe54f60ef97abf9091fe30eff5628a34a
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2024-05-09 11:51:12 +0800

    chore: add type rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,28 @@
 import typescript from "@rollup/plugin-typescript";
-
-export default {
-  input: "src/index.tsx",
-  output: [
-    {
-      file: './dist/index.js',
-      format: "cjs",
-    },
-    {
-      file: './dist/index.mjs',
-      format: "es",
-    },
-  ],
-  plugins: [typescript()],
-};
+import { dts } from "rollup-plugin-dts";
+export default [
+  {
+    input: "src/index.tsx",
+    output: [
+      {
+        file: "./dist/index.js",
+        format: "cjs",
+      },
+      {
+        file: "./dist/index.mjs",
+        format: "es",
+      },
+    ],
+    plugins: [typescript()],
+  },
+  {
+    input: "./dist/types/index.d.ts",
+    output: [
+      {
+        file: "./dist/index.d.ts",
+        format: "es",
+      },
+    ],
+    plugins: [dts()],
+  },
+];
