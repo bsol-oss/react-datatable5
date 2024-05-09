ae24b1ee07b6a79bbd3dc041c09e3d55c9d377d4
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2024-05-09 16:29:44 +0800

    fix: hide tooltip if label not exist

--- src/components/TextCell.tsx
+++ src/components/TextCell.tsx
@@ -1,17 +1,45 @@
 import { Tooltip, Text } from "@chakra-ui/react";
 
-export const TextCell = ({ label, children }: any) => {
-  return (
-    <Tooltip label={label}>
-      <Text
-        as="span"
-        overflow="hidden"
-        textOverflow={"ellipsis"}
-        noOfLines={[1, 2, 3]}
+export interface TextCellProps {
+  label?: string;
+  children: string | number | JSX.Element | JSX.Element[];
+}
+
+export const TextCell = ({ label, children }: TextCellProps) => {
+  if (label) {
+    return (
+      <Tooltip
+        label={
+          <Text
+            as="span"
+            overflow="hidden"
+            textOverflow={"ellipsis"}
+            noOfLines={[5]}
+          >
+            {label}
+          </Text>
+        }
+        placement="auto"
       >
-        {children}
-      </Text>
-    </Tooltip>
+        <Text
+          as="span"
+          overflow="hidden"
+          textOverflow={"ellipsis"}
+          noOfLines={[1, 2, 3]}
+        >
+          {children}
+        </Text>
+      </Tooltip>
+    );
+  }
+  return (
+    <Text
+      as="span"
+      overflow="hidden"
+      textOverflow={"ellipsis"}
+      noOfLines={[1, 2, 3]}
+    >
+      {children}
+    </Text>
   );
 };
-
