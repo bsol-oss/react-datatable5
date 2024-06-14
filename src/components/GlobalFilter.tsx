abaec77ea11e952e21352333fd414fae5480c88d
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2024-06-14 16:55:01 +0800

    feat: update globalfilter cosmetic

--- src/components/GlobalFilter.tsx
+++ src/components/GlobalFilter.tsx
@@ -1,15 +1,31 @@
-import { Input, Text, Box } from "@chakra-ui/react";
+import {
+  Box,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { MdSearch } from "react-icons/md";
 import { useDataTable } from "./useDataTable";
 
-export const GlobalFilter = () => {
+export const GlobalFilter = ({ icon = MdSearch }) => {
   const { globalFilter, setGlobalFilter } = useDataTable();
 
   return (
-    <Input
-      value={globalFilter}
-      onChange={(e) => {
-        setGlobalFilter(e.target.value);
-      }}
-    />
+    <>
+      <Box>
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <Icon as={icon} color="gray.300" />
+          </InputLeftElement>
+          <Input
+            value={globalFilter}
+            onChange={(e) => {
+              setGlobalFilter(e.target.value);
+            }}
+          />
+        </InputGroup>
+      </Box>
+    </>
   );
 };
