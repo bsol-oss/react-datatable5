215796d4d0d0f93dc054577748b0ae39a1462106
jacky-ng-w3b <147465107+jacky-ng-w3b@users.noreply.github.com>
2024-06-14 16:57:19 +0800

    Add default datatable (#4)
    * feat: update globalfilter cosmetic
    
    * feat: add default table

--- src/stories/DataTableServer/DataTableServer.stories.tsx
+++ src/stories/DataTableServer/DataTableServer.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import TableViewShowcase from "./TableViewShowcase";
 import CardViewShowcase from "./CardViewShowcase";
 import TablePinningShowcase from "./TablePinningShowcase";
+import DefaultTableShowcase from "./DefaultTableShowcase";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -14,6 +15,12 @@ const meta = {
 
 export default meta;
 type Story = StoryObj<typeof meta>;
+
+export const DefaultTableView: Story = {
+  render: () => {
+    return <DefaultTableShowcase />;
+  },
+};
 
 export const TableView: Story = {
   render: () => {
