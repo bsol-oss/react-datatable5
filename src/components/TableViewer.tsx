e473eef5f92df86fca01c0723bfe8f9a3c52f447
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2024-06-18 18:39:26 +0800

    feat: update table view to dragable reorder

--- src/components/TableViewer.tsx
+++ src/components/TableViewer.tsx
@@ -1,26 +1,89 @@
-import { Flex, Switch } from "@chakra-ui/react";
+import { Box, Flex, Grid, Icon, Switch } from "@chakra-ui/react";
+import { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { FaGripLinesVertical } from "react-icons/fa";
 import { useDataTable } from "./useDataTable";
 
 export const TableViewer = () => {
   const { table } = useDataTable();
+  const columns = table.getAllLeafColumns();
+  const [columnOrder, setColumnOrder] = useState(
+    columns.map((column, index) => {
+      return column.id;
+    })
+  );
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const newColumnOrder = Array.from(columnOrder);
+    const [removed] = newColumnOrder.splice(result.source.index, 1);
+    newColumnOrder.splice(result.destination.index, 0, removed);
+
+    setColumnOrder(newColumnOrder);
+    table.setColumnOrder(newColumnOrder);
+  };
 
   return (
-    <Flex flexFlow={"column"} gap={"1rem"}>
-      {table.getAllLeafColumns().map((column) => {
-        const displayName =
-          column.columnDef.meta === undefined
-            ? column.id
-            : column.columnDef.meta.displayName;
-        return (
-          <Flex flexFlow={"row"} gap={"0.5rem"} alignItems={"center"}>
-            <Switch
-              isChecked={column.getIsVisible()}
-              onChange={column.getToggleVisibilityHandler()}
-            />
-            {displayName}
-          </Flex>
-        );
-      })}
-    </Flex>
+    <>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="columns">
+          {(provided) => (
+            <Flex
+              flexFlow={"column"}
+              gap={"0.5rem"}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {columns.map((column, i) => {
+                const displayName =
+                  column.columnDef.meta === undefined
+                    ? column.id
+                    : column.columnDef.meta.displayName;
+                return (
+                  <>
+                    <Draggable
+                      key={column.id}
+                      draggableId={column.id}
+                      index={i}
+                    >
+                      {(provided) => (
+                        <Grid
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          templateColumns={"auto 1fr"}
+                          gap="0.5rem"
+                          alignItems={"center"}
+                        >
+                          <Flex
+                            {...provided.dragHandleProps}
+                            alignItems={"center"}
+                            padding={"auto 0 auto 0"}
+                          >
+                            {/* <FaGripLinesVertical /> */}
+                            <Icon as={FaGripLinesVertical} color={"gray.400"} />
+                          </Flex>
+                          <Flex
+                            justifyContent={"space-between"}
+                            alignItems={"center"}
+                          >
+                            <Box> {displayName}</Box>
+                            <Switch
+                              isChecked={column.getIsVisible()}
+                              onChange={column.getToggleVisibilityHandler()}
+                            />
+                          </Flex>
+                        </Grid>
+                      )}
+                    </Draggable>
+                  </>
+                );
+              })}
+              {provided.placeholder}
+            </Flex>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </>
   );
 };
