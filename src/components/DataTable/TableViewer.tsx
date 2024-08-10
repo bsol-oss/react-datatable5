80e6a96b74f42e6bd6bc7afee16e3153be200de2
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2024-08-10 18:41:10 +0800

    fix: ts

--- src/components/DataTable/TableViewer.tsx
+++ src/components/DataTable/TableViewer.tsx
@@ -1,19 +1,15 @@
 import { Box, Flex, Grid, Icon, Switch } from "@chakra-ui/react";
 import { useState } from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
 import { FaGripLinesVertical } from "react-icons/fa";
 import { useDataTableContext } from "./useDataTableContext";
 
 export const TableViewer = () => {
   const { table } = useDataTableContext();
   const columns = table.getAllLeafColumns();
-  const [columnOrder, setColumnOrder] = useState(
-    columns.map((column, index) => {
-      return column.id;
-    })
-  );
+  const [columnOrder, setColumnOrder] = useState<string[]>(columns.map(column => column.id));
 
-  const handleDragEnd = (result) => {
+  const handleDragEnd = (result: DropResult) => {
     if (!result.destination) return;
 
     const newColumnOrder = Array.from(columnOrder);
@@ -25,60 +21,51 @@ export const TableViewer = () => {
   };
 
   return (
-    <>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable droppableId="columns">
-          {(provided) => (
-            <Flex
-              flexFlow={"column"}
-              gap={"0.5rem"}
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-            >
-              {columns.map((column, i) => {
-                const displayName =
-                  column.columnDef.meta === undefined
-                    ? column.id
-                    : column.columnDef.meta.displayName;
-                return (
-                  <Draggable key={column.id} draggableId={column.id} index={i}>
-                    {(provided) => (
-                      <Grid
-                        key={column.id}
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        templateColumns={"auto 1fr"}
-                        gap="0.5rem"
-                        alignItems={"center"}
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <Droppable droppableId="columns">
+        {(provided) => (
+          <Flex
+            flexFlow="column"
+            gap="0.5rem"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {columns.map((column, index) => {
+              const displayName = column.columnDef.meta?.displayName || column.id;
+
+              return (
+                <Draggable key={column.id} draggableId={column.id} index={index}>
+                  {(provided) => (
+                    <Grid
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      templateColumns="auto 1fr"
+                      gap="0.5rem"
+                      alignItems="center"
+                    >
+                      <Flex
+                        {...provided.dragHandleProps}
+                        alignItems="center"
+                        padding="0"
                       >
-                        <Flex
-                          {...provided.dragHandleProps}
-                          alignItems={"center"}
-                          padding={"auto 0 auto 0"}
-                        >
-                          {/* <FaGripLinesVertical /> */}
-                          <Icon as={FaGripLinesVertical} color={"gray.400"} />
-                        </Flex>
-                        <Flex
-                          justifyContent={"space-between"}
-                          alignItems={"center"}
-                        >
-                          <Box> {displayName}</Box>
-                          <Switch
-                            isChecked={column.getIsVisible()}
-                            onChange={column.getToggleVisibilityHandler()}
-                          />
-                        </Flex>
-                      </Grid>
-                    )}
-                  </Draggable>
-                );
-              })}
-              {provided.placeholder}
-            </Flex>
-          )}
-        </Droppable>
-      </DragDropContext>
-    </>
+                        <Icon as={FaGripLinesVertical} color="gray.400" />
+                      </Flex>
+                      <Flex justifyContent="space-between" alignItems="center">
+                        <Box>{displayName}</Box>
+                        <Switch
+                          isChecked={column.getIsVisible()}
+                          onChange={column.getToggleVisibilityHandler()}
+                        />
+                      </Flex>
+                    </Grid>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </Flex>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
