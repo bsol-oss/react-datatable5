7e86dd7013f6ae9e023e58ab545fad9537c225b2
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2024-11-08 14:34:47 +0800

    feat: update card view style

--- src/components/DataTable/TableCards.tsx
+++ src/components/DataTable/TableCards.tsx
@@ -1,4 +1,11 @@
-import { Box, Card, CardBody, Checkbox } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  Checkbox,
+  Grid,
+  Text
+} from "@chakra-ui/react";
 import { flexRender } from "@tanstack/react-table";
 import { useContext } from "react";
 import { TableContext } from "./DataTableContext";
@@ -26,13 +33,29 @@ export const TableCards = ({ isSelectable = false }: TableCardsProps) => {
                   }}
                 ></Checkbox>
               )}
-              {row.getVisibleCells().map((cell) => {
-                return (
-                  <Box key={`chakra-table-cardcolumn-${row.id}`}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </Box>
-                );
-              })}
+              <Grid templateColumns={"auto 1fr"} gap={"1rem"}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <>
+                      <Box key={`chakra-table-cardcolumnid-${row.id}`}>
+                        <Text fontWeight={"bold"}>
+                          {cell.column.columnDef.meta?.displayName ??
+                            cell.column.id}
+                        </Text>
+                      </Box>
+                      <Box
+                        key={`chakra-table-cardcolumn-${row.id}`}
+                        justifySelf={"end"}
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </Box>
+                    </>
+                  );
+                })}
+              </Grid>
             </CardBody>
           </Card>
         );
