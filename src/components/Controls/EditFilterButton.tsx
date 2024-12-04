16e058613842d69b16ad7c5bf7e55cb67dfe06be
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2024-12-04 14:36:29 +0800

    feat: update editFilter to v3

--- src/components/Controls/EditFilterButton.tsx
+++ src/components/Controls/EditFilterButton.tsx
@@ -1,9 +1,4 @@
-import {
-  Button,
-  Flex,
-  IconButton,
-  useDisclosure
-} from "@chakra-ui/react";
+import { Button, Flex, IconButton, useDisclosure } from "@chakra-ui/react";
 import { MdFilterAlt } from "react-icons/md";
 import { ResetFilteringButton, TableFilter } from "../../index";
 
@@ -15,7 +10,9 @@ import {
   DialogRoot,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+  DialogFooter,
+  DialogActionTrigger,
+} from "@/components/ui/dialog";
 
 export interface EditFilterButtonProps {
   text?: string;
@@ -31,52 +28,37 @@ export const EditFilterButton = ({
   closeText = "Close",
   resetText = "Reset",
   icon = <MdFilterAlt />,
-  ...props
 }: EditFilterButtonProps) => {
   const filterModal = useDisclosure();
   return (
     <>
-      {!!text === false && (
-        <IconButton
-          icon={icon}
-          variant={"ghost"}
-          onClick={filterModal.onOpen}
-          aria-label={"filter"}
-          {...props}
-        />
-      )}
-      {!!text !== false && (
-        <Button
-          leftIcon={icon}
-          variant={"ghost"}
-          onClick={filterModal.onOpen}
-          {...props}
-        >
-          {text}
-        </Button>
-      )}
-
-      <Modal
-        isOpen={filterModal.isOpen}
-        onClose={filterModal.onClose}
-        size={["full", "full", "md", "md"]}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{title}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Flex flexFlow={"column"} gap={"1rem"}>
-              <TableFilter />
-              <ResetFilteringButton text={resetText} />
-            </Flex>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button onClick={filterModal.onClose}>{closeText}</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <DialogRoot size={["full", "full", "md", "md"]}>
+        <DialogRoot>
+          <DialogTrigger asChild>
+            <Button variant="outline">
+              {icon} {text}
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>{title}</DialogTitle>
+            </DialogHeader>
+            <DialogBody>
+              <Flex flexFlow={"column"} gap={"1rem"}>
+                <TableFilter />
+                <ResetFilteringButton text={resetText} />
+              </Flex>
+            </DialogBody>
+            <DialogFooter>
+              <DialogActionTrigger asChild>
+                <Button onClick={filterModal.onClose}>{closeText}</Button>
+              </DialogActionTrigger>
+              <Button>Save</Button>
+            </DialogFooter>
+            <DialogCloseTrigger />
+          </DialogContent>
+        </DialogRoot>
+      </DialogRoot>
     </>
   );
 };
