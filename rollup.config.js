2750a324f2abb54cb1fe92ba5e89c1d68c578124
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2025-02-24 14:04:37 +0800

    fix: atlanssian import esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,13 @@
 import typescript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
+
+import alias from '@rollup/plugin-alias';
+import path from 'path';
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const root = path.resolve(__dirname);
 export default [
   {
     input: "src/index.tsx",
@@ -13,7 +21,20 @@ export default [
         format: "es",
       },
     ],
-    plugins: [typescript({ exclude: "./src/stories/**/*" })],
+    plugins: [
+      typescript({ exclude: "./src/stories/**/*" }),
+      alias({
+        entries: [
+          {
+            find: "@atlaskit/pragmatic-drag-and-drop",
+            replacement: path.resolve(
+              root,
+              "./node_modules/@atlaskit/pragmatic-drag-and-drop/dist/esm/entry-point"
+            ),
+          },
+        ],
+      }),
+    ],
   },
   {
     input: "./dist/types/index.d.ts",
