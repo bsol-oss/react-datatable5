b7ff74da424e885df869d22a50acdbcf3fb5c9c5
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2025-03-06 17:27:18 +0800

    chore: add event geolocations stories

---
+++ src/stories/Form/EventsGeolocations.stories.tsx
@@ -0,0 +1,170 @@
+import { Form } from "@/components/Form/Form";
+import type { Meta, StoryObj } from "@storybook/react";
+import { JSONSchema7 } from "json-schema";
+import { eventsSchema } from "../schema";
+import { Provider } from "@/components/ui/provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useForm } from "@/components/Form/useForm";
+import { I18nextProvider, initReactI18next } from "react-i18next";
+import i18n from "i18next";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "react-datatable5/Form",
+  component: Form,
+  parameters: {},
+
+  argTypes: {},
+} satisfies Meta<typeof Form>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+const queryClient = new QueryClient();
+
+i18n
+  .use(initReactI18next) // bind react-i18next to the instance
+  .init({
+    fallbackLng: "en",
+    debug: true,
+
+    interpolation: {
+      escapeValue: false, // not needed for react!!
+    },
+  });
+
+export const EventsGeolocations: Story = {
+  render: () => {
+    return (
+      <Provider>
+        <QueryClientProvider client={queryClient}>
+          <I18nextProvider i18n={i18n} defaultNS={"translation"}>
+            <SomeForm />
+          </I18nextProvider>
+        </QueryClientProvider>
+      </Provider>
+    );
+  },
+};
+
+const eventsGeolocationsSchema = {
+  title: "events_geolocations",
+  properties: {
+    parent_id: {
+      type: "string",
+      format: "uuid",
+    },
+    child_records: {
+      type: "array",
+      items: {
+        $id: "core_geolocations",
+        type: "object",
+        title: "core_geolocations",
+        $schema: "http://json-schema.org/draft-07/schema#",
+        example: {
+          name: "Excepteur",
+        },
+        required: ["name"],
+        properties: {
+          id: {
+            type: "string",
+            format: "uuid",
+            description:
+              "Missing description. Database type: uuid. Default value: uuid_generate_v4()",
+          },
+          city: {
+            type: "string",
+            maxLength: 255,
+            description:
+              "Missing description. Database type: character varying. Default value: null",
+          },
+          name: {
+            type: "string",
+            maxLength: 255,
+            description:
+              "Missing description. Database type: character varying. Default value: null",
+          },
+          region: {
+            type: "string",
+            maxLength: 255,
+            description:
+              "Missing description. Database type: character varying. Default value: null",
+          },
+          street: {
+            type: "string",
+            maxLength: 255,
+            description:
+              "Missing description. Database type: character varying. Default value: null",
+          },
+          country: {
+            type: "string",
+            maxLength: 255,
+            description:
+              "Missing description. Database type: character varying. Default value: null",
+          },
+          district: {
+            type: "string",
+            maxLength: 255,
+            description:
+              "Missing description. Database type: character varying. Default value: null",
+          },
+          latitude: {
+            type: "number",
+            description:
+              "Missing description. Database type: double precision. Default value: null",
+          },
+          longitude: {
+            type: "number",
+            description:
+              "Missing description. Database type: double precision. Default value: null",
+          },
+          created_at: {
+            type: "string",
+            format: "date-time",
+            description:
+              "Missing description. Database type: timestamp with time zone. Default value: CURRENT_TIMESTAMP",
+          },
+          deleted_at: {
+            type: "string",
+            format: "date-time",
+            description:
+              "Missing description. Database type: timestamp with time zone. Default value: null",
+          },
+          extra_info: {
+            type: "object",
+            properties: {},
+            description:
+              "Missing description. Database type: jsonb. Default value: null",
+          },
+          updated_at: {
+            type: "string",
+            format: "date-time",
+            description:
+              "Missing description. Database type: timestamp with time zone. Default value: CURRENT_TIMESTAMP",
+          },
+          description: {
+            type: "string",
+            description:
+              "Missing description. Database type: text. Default value: null",
+          },
+        },
+        description: "Missing description",
+        searchingColumns: ["name"],
+        additionalProperties: false,
+      },
+    },
+  },
+};
+
+const SomeForm = () => {
+  const form = useForm({ keyPrefix: "nice" });
+
+  return (
+    <Form
+      schema={eventsGeolocationsSchema as JSONSchema7}
+      // ignore={["id", "created_at", "updated_at"]}
+      serverUrl={"http://localhost:8081"}
+      {...form}
+    />
+  );
+};
