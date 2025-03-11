af7d963980d63b52dff8d2168590146297e89022
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2025-03-11 12:06:05 +0800

    refactor form body

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,39 +1,18 @@
 import { SchemaFormContext } from "@/components/Form/SchemaFormContext";
 import { ForeignKeyProps } from "@/components/Form/components/fields/StringInputField";
-import { useSchemaContext } from "@/components/Form/useSchemaContext";
-import { clearEmptyString } from "@/components/Form/utils/clearEmptyString";
-import { idListSanityCheck } from "@/components/Form/utils/idListSanityCheck";
-import {
-  AccordionItem,
-  AccordionItemContent,
-  AccordionItemTrigger,
-  AccordionRoot,
-} from "@/components/ui/accordion";
-import { Button } from "@/components/ui/button";
-import {
-  Alert,
-  Box,
-  Center,
-  Flex,
-  Grid,
-  Heading,
-  Spinner,
-} from "@chakra-ui/react";
-import axios, { AxiosRequestConfig } from "axios";
+import { Grid } from "@chakra-ui/react";
+import { AxiosRequestConfig } from "axios";
 import { JSONSchema7 } from "json-schema";
 import { Dispatch, SetStateAction, useState } from "react";
 import {
   FieldValues,
   FormProvider,
   SubmitHandler,
-  useFormContext,
   UseFormReturn,
 } from "react-hook-form";
 import { UseTranslationResponse } from "react-i18next";
-import { ColumnRenderer } from "./components/fields/ColumnRenderer";
-import { ColumnViewer } from "./components/viewers/ColumnViewer";
-import { SubmitButton } from "./components/core/SubmitButton";
 import { FormTitle } from "./components/core/FormTitle";
+import { FormBody } from "./components/core/FormBody";
 
 export interface FormProps<TData extends FieldValues> {
   schema: JSONSchema7;
@@ -94,233 +73,11 @@ export const idPickerSanityCheck = (
 };
 
 const FormInternal = <TData extends FieldValues>() => {
-  const {
-    schema,
-    requestUrl,
-    order,
-    ignore,
-    include,
-    onSubmit,
-    rowNumber,
-    translate,
-    requestOptions,
-    isSuccess,
-    setIsSuccess,
-    isError,
-    setIsError,
-    isSubmiting,
-    setIsSubmiting,
-    isConfirming,
-    setIsConfirming,
-    validatedData,
-    setValidatedData,
-    error,
-    setError,
-  } = useSchemaContext();
-
-  const methods = useFormContext();
-
-  const { properties } = schema;
-
-  const onBeforeSubmit = () => {
-    setIsSubmiting(true);
-  };
-  const onAfterSubmit = () => {
-    setIsSubmiting(false);
-  };
-  const onSubmitError = () => {
-    setIsError(true);
-  };
-  const onSubmitSuccess = () => {
-    setIsSuccess(true);
-  };
-  const defaultOnSubmit = async (promise: Promise<unknown>) => {
-    try {
-      onBeforeSubmit();
-      await promise;
-      onSubmitSuccess();
-    } catch (error) {
-      setIsError(true);
-      setError(error);
-      onSubmitError();
-    } finally {
-      onAfterSubmit();
-    }
-  };
-  const defaultSubmitPromise = (data: TData) => {
-    const options = {
-      method: "POST",
-      url: `${requestUrl}`,
-      data: clearEmptyString(data),
-      ...requestOptions,
-    };
-    return axios.request(options);
-  };
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const onFormSubmit = async (data: any) => {
-    if (onSubmit === undefined) {
-      await defaultOnSubmit(defaultSubmitPromise(data));
-      return;
-    }
-    await defaultOnSubmit(onSubmit(data));
-  };
-
-  interface renderColumnsConfig {
-    order: string[];
-    keys: string[];
-    ignore: string[];
-    include: string[];
-  }
-
-  const renderColumns = ({
-    order,
-    keys,
-    ignore,
-    include,
-  }: renderColumnsConfig) => {
-    const included = include.length > 0 ? include : keys;
-    const not_exist = included.filter(
-      (columnA) => !order.some((columnB) => columnA === columnB)
-    );
-    const ordered = [...order, ...not_exist];
-    const ignored = ordered.filter(
-      (column) => !ignore.some((shouldIgnore) => column === shouldIgnore)
-    );
-    return ignored;
-  };
-
-  const ordered = renderColumns({
-    order,
-    keys: Object.keys(properties as object),
-    ignore,
-    include,
-  });
-
-  if (isSuccess) {
-    return (
-      <Grid gap={2}>
-        <FormTitle />
-        <Alert.Root status="success">
-          <Alert.Indicator />
-          <Alert.Title>{translate.t("submitSuccess")}</Alert.Title>
-        </Alert.Root>
-        <Flex justifyContent={"end"}>
-          <Button
-            onClick={() => {
-              setIsError(false);
-              setIsSubmiting(false);
-              setIsSuccess(false);
-              setIsConfirming(false);
-              setValidatedData(undefined);
-              methods.reset();
-            }}
-            formNoValidate
-          >
-            {translate.t("submitAgain")}
-          </Button>
-        </Flex>
-      </Grid>
-    );
-  }
-  if (isConfirming) {
-    return (
-      <Grid gap={2}>
-        <FormTitle />
-        <Grid
-          gap={4}
-          gridTemplateColumns={"repeat(12, 1fr)"}
-          gridTemplateRows={`repeat(${rowNumber ?? "auto-fit"}, auto)`}
-        >
-          {ordered.map((column) => {
-            return (
-              <ColumnViewer
-                // @ts-expect-error find suitable types
-                properties={properties}
-                prefix={``}
-                key={`form-viewer-${column}`}
-                {...{ column }}
-              />
-            );
-          })}
-        </Grid>
-        <Flex justifyContent={"end"} gap={"2"}>
-          <Button
-            onClick={() => {
-              setIsConfirming(false);
-            }}
-            variant={"subtle"}
-          >
-            {translate.t("cancel")}
-          </Button>
-          <Button
-            onClick={() => {
-              onFormSubmit(validatedData);
-            }}
-          >
-            {translate.t("confirm")}
-          </Button>
-        </Flex>
-
-        {isSubmiting && (
-          <Box pos="absolute" inset="0" bg="bg/80">
-            <Center h="full">
-              <Spinner color="teal.500" />
-            </Center>
-          </Box>
-        )}
-        {isError && (
-          <>
-            <Alert.Root status="error">
-              <Alert.Title>
-                <AccordionRoot collapsible defaultValue={["b"]}>
-                  <AccordionItem value={"b"}>
-                    <AccordionItemTrigger>
-                      <Alert.Indicator />
-                      {`${error}`}
-                    </AccordionItemTrigger>
-                    <AccordionItemContent>{`${JSON.stringify(error)}`}</AccordionItemContent>
-                  </AccordionItem>
-                </AccordionRoot>
-              </Alert.Title>
-            </Alert.Root>
-          </>
-        )}
-      </Grid>
-    );
-  }
-
   return (
     <>
       <Grid gap="2">
         <FormTitle />
-        <Grid
-          gap="4"
-          gridTemplateColumns={"repeat(12, 1fr)"}
-          gridTemplateRows={`repeat(${rowNumber ?? "auto-fit"}, auto)`}
-        >
-          {ordered.map((column) => {
-            return (
-              <ColumnRenderer
-                // @ts-expect-error find suitable types
-                properties={properties}
-                prefix={``}
-                key={`form-input-${column}`}
-                {...{ column }}
-              />
-            );
-          })}
-        </Grid>
-        <Flex justifyContent={"end"} gap="2">
-          <Button
-            onClick={() => {
-              methods.reset();
-            }}
-            variant={"subtle"}
-          >
-            {translate.t("reset")}
-          </Button>
-          <SubmitButton />
-        </Flex>
+        <FormBody />
       </Grid>
     </>
   );
---
+++ src/components/Form/components/core/FormBody.tsx
@@ -0,0 +1,249 @@
+import {
+  Alert,
+  Box,
+  Button,
+  Center,
+  Flex,
+  Grid,
+  Spinner,
+} from "@chakra-ui/react";
+import { ColumnRenderer } from "../fields/ColumnRenderer";
+import { SubmitButton } from "./SubmitButton";
+import axios from "axios";
+import { useFormContext } from "react-hook-form";
+import { useSchemaContext } from "../../useSchemaContext";
+import { clearEmptyString } from "../../utils/clearEmptyString";
+import { ColumnViewer } from "../viewers/ColumnViewer";
+import {
+  AccordionItem,
+  AccordionItemContent,
+  AccordionItemTrigger,
+  AccordionRoot,
+} from "@/components/ui/accordion";
+
+export const FormBody = () => {
+  const {
+    schema,
+    requestUrl,
+    order,
+    ignore,
+    include,
+    onSubmit,
+    rowNumber,
+    translate,
+    requestOptions,
+    isSuccess,
+    setIsSuccess,
+    isError,
+    setIsError,
+    isSubmiting,
+    setIsSubmiting,
+    isConfirming,
+    setIsConfirming,
+    validatedData,
+    setValidatedData,
+    error,
+    setError,
+  } = useSchemaContext();
+
+  const methods = useFormContext();
+
+  const { properties } = schema;
+
+  const onBeforeSubmit = () => {
+    setIsSubmiting(true);
+  };
+  const onAfterSubmit = () => {
+    setIsSubmiting(false);
+  };
+  const onSubmitError = () => {
+    setIsError(true);
+  };
+  const onSubmitSuccess = () => {
+    setIsSuccess(true);
+  };
+  const defaultOnSubmit = async (promise: Promise<unknown>) => {
+    try {
+      onBeforeSubmit();
+      await promise;
+      onSubmitSuccess();
+    } catch (error) {
+      setIsError(true);
+      setError(error);
+      onSubmitError();
+    } finally {
+      onAfterSubmit();
+    }
+  };
+  const defaultSubmitPromise = (data: TData) => {
+    const options = {
+      method: "POST",
+      url: `${requestUrl}`,
+      data: clearEmptyString(data),
+      ...requestOptions,
+    };
+    return axios.request(options);
+  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onFormSubmit = async (data: any) => {
+    if (onSubmit === undefined) {
+      await defaultOnSubmit(defaultSubmitPromise(data));
+      return;
+    }
+    await defaultOnSubmit(onSubmit(data));
+  };
+
+  interface renderColumnsConfig {
+    order: string[];
+    keys: string[];
+    ignore: string[];
+    include: string[];
+  }
+
+  const renderColumns = ({
+    order,
+    keys,
+    ignore,
+    include,
+  }: renderColumnsConfig) => {
+    const included = include.length > 0 ? include : keys;
+    const not_exist = included.filter(
+      (columnA) => !order.some((columnB) => columnA === columnB)
+    );
+    const ordered = [...order, ...not_exist];
+    const ignored = ordered.filter(
+      (column) => !ignore.some((shouldIgnore) => column === shouldIgnore)
+    );
+    return ignored;
+  };
+
+  const ordered = renderColumns({
+    order,
+    keys: Object.keys(properties as object),
+    ignore,
+    include,
+  });
+
+  if (isSuccess) {
+    return (
+      <Flex flexFlow={"column"} gap="2">
+        <Alert.Root status="success">
+          <Alert.Indicator />
+          <Alert.Title>{translate.t("submitSuccess")}</Alert.Title>
+        </Alert.Root>
+        <Flex justifyContent={"end"}>
+          <Button
+            onClick={() => {
+              setIsError(false);
+              setIsSubmiting(false);
+              setIsSuccess(false);
+              setIsConfirming(false);
+              setValidatedData(undefined);
+              methods.reset();
+            }}
+            formNoValidate
+          >
+            {translate.t("submitAgain")}
+          </Button>
+        </Flex>
+      </Flex>
+    );
+  }
+  if (isConfirming) {
+    return (
+      <Flex flexFlow={"column"} gap="2">
+        <Grid
+          gap={4}
+          gridTemplateColumns={"repeat(12, 1fr)"}
+          gridTemplateRows={`repeat(${rowNumber ?? "auto-fit"}, auto)`}
+        >
+          {ordered.map((column) => {
+            return (
+              <ColumnViewer
+                // @ts-expect-error find suitable types
+                properties={properties}
+                prefix={``}
+                key={`form-viewer-${column}`}
+                {...{ column }}
+              />
+            );
+          })}
+        </Grid>
+        <Flex justifyContent={"end"} gap={"2"}>
+          <Button
+            onClick={() => {
+              setIsConfirming(false);
+            }}
+            variant={"subtle"}
+          >
+            {translate.t("cancel")}
+          </Button>
+          <Button
+            onClick={() => {
+              onFormSubmit(validatedData);
+            }}
+          >
+            {translate.t("confirm")}
+          </Button>
+        </Flex>
+
+        {isSubmiting && (
+          <Box pos="absolute" inset="0" bg="bg/80">
+            <Center h="full">
+              <Spinner color="teal.500" />
+            </Center>
+          </Box>
+        )}
+        {isError && (
+          <>
+            <Alert.Root status="error">
+              <Alert.Title>
+                <AccordionRoot collapsible defaultValue={["b"]}>
+                  <AccordionItem value={"b"}>
+                    <AccordionItemTrigger>
+                      <Alert.Indicator />
+                      {`${error}`}
+                    </AccordionItemTrigger>
+                    <AccordionItemContent>{`${JSON.stringify(error)}`}</AccordionItemContent>
+                  </AccordionItem>
+                </AccordionRoot>
+              </Alert.Title>
+            </Alert.Root>
+          </>
+        )}
+      </Flex>
+    );
+  }
+  return (
+    <Flex flexFlow={"column"} gap="2">
+      <Grid
+        gap="4"
+        gridTemplateColumns={"repeat(12, 1fr)"}
+        gridTemplateRows={`repeat(${rowNumber ?? "auto-fit"}, auto)`}
+      >
+        {ordered.map((column) => {
+          return (
+            <ColumnRenderer
+              // @ts-expect-error find suitable types
+              properties={properties}
+              prefix={``}
+              key={`form-input-${column}`}
+              {...{ column }}
+            />
+          );
+        })}
+      </Grid>
+      <Flex justifyContent={"end"} gap="2">
+        <Button
+          onClick={() => {
+            methods.reset();
+          }}
+          variant={"subtle"}
+        >
+          {translate.t("reset")}
+        </Button>
+        <SubmitButton />
+      </Flex>
+    </Flex>
+  );
+};
