d00c5790f47745f1d1996fbf61046287917d7eea
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2025-04-24 17:40:15 +0800

    feat: file viewer

--- src/components/Form/components/viewers/FileViewer.tsx
+++ src/components/Form/components/viewers/FileViewer.tsx
@@ -1,18 +1,11 @@
 import { Field } from "@/components/ui/field";
-import { Box, Card, Flex, Text } from "@chakra-ui/react";
+import { Box, Card, Flex, Image } from "@chakra-ui/react";
 import { useFormContext } from "react-hook-form";
-import { TiDeleteOutline } from "react-icons/ti";
 import { useSchemaContext } from "../../useSchemaContext";
-import { FileDropzone } from "../FileDropzone";
 import { CustomJSONSchema7 } from "../types/CustomJSONSchema7";
-import { removeIndex } from "../../utils/removeIndex";
 
 export const FileViewer = ({ column, schema, prefix }) => {
-  const {
-    setValue,
-    formState: { errors },
-    watch,
-  } = useFormContext();
+  const { watch } = useFormContext();
   const { translate } = useSchemaContext();
   const { required, gridColumn, gridRow } = schema as CustomJSONSchema7;
   const isRequired = required?.some((columnId) => columnId === column);
@@ -29,47 +22,33 @@ export const FileViewer = ({ column, schema, prefix }) => {
       gridTemplateRows={"auto 1fr auto"}
       alignItems={"stretch"}
     >
-      <FileDropzone
-        onDrop={({ files }) => {
-          const newFiles = files.filter(
-            ({ name }) => !currentFiles.some((cur) => cur.name === name)
-          );
-          setValue(colLabel, [...currentFiles, ...newFiles]);
-        }}
-        placeholder={translate.t(`${colLabel}.fileDropzone`)}
-      />
       <Flex flexFlow={"column"} gap={1}>
         {currentFiles.map((file) => {
           return (
             <Card.Root variant={"subtle"} key={file.name}>
               <Card.Body
                 gap="2"
-                cursor={"pointer"}
-                onClick={() => {
-                  setValue(
-                    column,
-                    currentFiles.filter(({ name }) => {
-                      return name !== file.name;
-                    })
-                  );
-                }}
                 display={"flex"}
                 flexFlow={"row"}
                 alignItems={"center"}
                 padding={"2"}
               >
+                {file.type.startsWith("image/") && (
+                  <Image
+                    src={URL.createObjectURL(file)}
+                    alt={file.name}
+                    boxSize="50px"
+                    objectFit="cover"
+                    borderRadius="md"
+                    marginRight="2"
+                  />
+                )}
                 <Box>{file.name}</Box>
-                <TiDeleteOutline />
               </Card.Body>
             </Card.Root>
           );
         })}
       </Flex>
-      {errors[`${colLabel}`] && (
-        <Text color={"red.400"}>
-          {translate.t(removeIndex(`${colLabel}.field_required`))}
-        </Text>
-      )}
     </Field>
   );
 };
