eb594a421e53f4e5fcb8c897e9e8bf3cbe3f665a
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2025-04-29 11:03:52 +0800

    fix: ts

--- src/components/DataTable/context/DataTableContext.tsx
+++ src/components/DataTable/context/DataTableContext.tsx
@@ -1,4 +1,7 @@
-import { OnChangeFn, Table } from "@tanstack/react-table";
+import {
+  OnChangeFn,
+  Table
+} from "@tanstack/react-table";
 import { createContext } from "react";
 import { UseTranslationResponse } from "react-i18next";
 import { DataTableProps } from "../DataTable";
@@ -8,7 +11,7 @@ export interface DataTableContext<TData = unknown> extends DataTableProps {
   globalFilter: string;
   setGlobalFilter: OnChangeFn<string>;
   type: "client" | "server";
-  translate: UseTranslationResponse<any, any>;
+  translate: UseTranslationResponse<any, unknown>;
 }
 
 export const DataTableContext = createContext<DataTableContext>({
@@ -16,5 +19,38 @@ export const DataTableContext = createContext<DataTableContext>({
   globalFilter: "",
   setGlobalFilter: () => {},
   type: "client",
-  translate: {} as UseTranslationResponse<any, any>,
+  translate: {} as UseTranslationResponse<any, unknown>,
+  data: [],
+  columns: [],
+  columnOrder: [],
+  columnFilters: [],
+  density: "sm",
+  sorting: [],
+  setPagination: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  setSorting: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  setColumnFilters: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  setRowSelection: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  setColumnOrder: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  setDensity: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  setColumnVisibility: function (): void {
+    throw new Error("Function not implemented.");
+  },
+  pagination: {
+    pageIndex: 0,
+    pageSize: 10,
+  },
+  rowSelection: {},
+  columnVisibility: {},
 });
