c9151941e8225f82134468fc9ae09ecd552fceda
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2025-05-27 12:25:48 +0800

    fix: init tableLabel

--- src/components/DataTable/context/DataTableContext.tsx
+++ src/components/DataTable/context/DataTableContext.tsx
@@ -70,5 +70,15 @@ export const DataTableContext = createContext<DataTableContext>({
   tableLabel: {
     view: "View",
     edit: "Edit",
+    filterButtonText: "Filter",
+    filterTitle: "Filter",
+    filterReset: "Reset",
+    filterClose: "Close",
+    reloadTooltip: "Reload",
+    reloadButtonText: "Reload",
+    resetSelection: "Reset Selection",
+    resetSorting: "Reset Sorting",
+    rowCountText: "Row Count",
+    hasErrorText: "Has Error",
   },
 });
